f68db4869b8e4873cbb481f3412d58ae9d1fbc5e
qisashasanudin <qisas.tazkia@ui.ac.id>
2020-10-05 15:44:02 +0700

    ch2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,19 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
+
+var BGColor = Colors.blue;
+var AcColor = Colors.white;
 
 void main() {
   // WidgetsApp // MaterialApp // CupertinoApp
   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
     home: HomePage(),
     theme: ThemeData(
-      primarySwatch: Colors.yellow,
+      primarySwatch: BGColor,
     ),
   ));
 }
@@ -15,12 +21,83 @@ void main() {
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Belajar Flutter"),
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+        statusBarBrightness: Brightness.dark,
+        statusBarIconBrightness: Brightness.dark,
+        systemNavigationBarColor: Colors.transparent,
+        systemNavigationBarIconBrightness: Brightness.dark,
       ),
-      body: Container(
-        child: Text("Hello World"),
+      child: Scaffold(
+        extendBodyBehindAppBar: true,
+        backgroundColor: BGColor,
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: Colors.transparent,
+          title: Text("Qisas Hasanudin"),
+        ),
+        body: Container(
+          margin: const EdgeInsets.only(top: 90, left: 20, right: 20),
+          padding: const EdgeInsets.all(20),
+          decoration: BoxDecoration(
+            color: AcColor,
+            borderRadius: BorderRadius.circular(20),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey[700],
+                blurRadius: 10,
+                offset: Offset(0, 3),
+              ),
+            ],
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                padding: const EdgeInsets.all(8),
+                width: 100,
+                height: 100,
+                color: Colors.red,
+                alignment: Alignment.center,
+              ),
+              Container(
+                padding: const EdgeInsets.all(8),
+                width: 100,
+                height: 100,
+                color: Colors.yellow,
+                alignment: Alignment.center,
+              ),
+              Container(
+                padding: const EdgeInsets.all(8),
+                width: 100,
+                height: 100,
+                color: Colors.green,
+                alignment: Alignment.center,
+              ),
+            ],
+          ),
+        ),
+        bottomNavigationBar: CurvedNavigationBar(
+          color: AcColor,
+          backgroundColor: BGColor,
+          buttonBackgroundColor: AcColor,
+          height: 50,
+          items: <Widget>[
+            Icon(Icons.list, size: 30, color: Colors.grey),
+            Icon(Icons.favorite, size: 30, color: Colors.grey),
+            Icon(Icons.add, size: 30, color: Colors.grey),
+            Icon(Icons.calendar_today, size: 30, color: Colors.grey),
+            Icon(Icons.settings, size: 30, color: Colors.grey),
+          ],
+          animationDuration: Duration(milliseconds: 200),
+          animationCurve: Curves.bounceInOut,
+          index: 2,
+          onTap: (index) {
+            debugPrint("Current index is $index");
+          },
+        ),
       ),
     );
   }
